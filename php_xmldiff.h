b85c1754a1f8de9f6beebcbd3ba99d814f86d6a9
Derick Rethans <github@derickrethans.nl>
2024-11-27 10:54:08 +0000

    release: Back to dev

--- php_xmldiff.h
+++ php_xmldiff.h
@@ -110,7 +110,7 @@ struct ze_xmldiff_obj {
 };
 #endif
 
-#define PHP_XMLDIFF_VERSION "1.1.4"
+#define PHP_XMLDIFF_VERSION "1.1.5-dev"
 
 PHP_MINIT_FUNCTION(xmldiff);
 PHP_MSHUTDOWN_FUNCTION(xmldiff);
